bf264b35e5253e9159cae2a2cda726d7122ecfb3
Manuel Schmidt <manu.schmidt.14@gmail.com>
2021-04-19 21:59:44 +0200

    Finished Temporal Difference Part 1

--- temporal-difference/check_test.py
+++ temporal-difference/check_test.py
@@ -1,11 +1,13 @@
-import unittest 
+import unittest
 from IPython.display import Markdown, display
 import numpy as np
+
 
 def printmd(string):
     display(Markdown(string))
 
-V_opt = np.zeros((4,12))
+
+V_opt = np.zeros((4, 12))
 V_opt[0:13][0] = -np.arange(3, 15)[::-1]
 V_opt[0:13][1] = -np.arange(3, 15)[::-1] + 1
 V_opt[0:13][2] = -np.arange(3, 15)[::-1] + 2
@@ -13,18 +15,21 @@ V_opt[3][0] = -13
 
 pol_opt = np.hstack((np.ones(11), 2, 0))
 
-V_true = np.zeros((4,12))
+V_true = np.zeros((4, 12))
 for i in range(3):
     V_true[0:13][i] = -np.arange(3, 15)[::-1] - i
 V_true[1][11] = -2
 V_true[2][11] = -1
 V_true[3][0] = -17
 
+
 def get_long_path(V):
     return np.array(np.hstack((V[0:13][0], V[1][0], V[1][11], V[2][0], V[2][11], V[3][0], V[3][11])))
 
+
 def get_optimal_path(policy):
     return np.array(np.hstack((policy[2][:], policy[3][0])))
+
 
 class Tests(unittest.TestCase):
 
@@ -37,12 +42,14 @@ class Tests(unittest.TestCase):
         to_check = get_optimal_path(policy)
         np.testing.assert_equal(pol_opt, to_check)
 
+
 check = Tests()
+
 
 def run_check(check_name, func):
     try:
         getattr(check, check_name)(func)
     except check.failureException as e:
-        printmd('**<span style="color: red;">PLEASE TRY AGAIN</span>**')
+        print('PLEASE TRY AGAIN')
         return
-    printmd('**<span style="color: green;">PASSED</span>**')
+    print('PASSED')
