ee75c6d6b256841331de574e0f0817a0c26ae412
Manuel Schmidt <manu.schmidt.14@gmail.com>
2021-04-20 08:40:13 +0200

    Finished Temporal Difference Part 3

--- temporal-difference/Temporal_Difference_Part3.py
+++ temporal-difference/Temporal_Difference_Part3.py
@@ -11,7 +11,7 @@ from plot_utils import plot_values
 env: CliffWalkingEnv = gym.make('CliffWalking-v0')
 
 
-def q_learning(env, num_episodes, alpha, gamma=1.0, eps_start=1.0, eps_decay=.9999, eps_min=0.05) -> tuple:
+def expected_sarsa(env, num_episodes, alpha, gamma=1.0, eps_start=1.0, eps_decay=.9999, eps_min=0.05) -> tuple:
     # initialize action-value function (empty dictionary of arrays)
     Q = defaultdict(lambda: np.zeros(env.nA))
 
@@ -23,7 +23,7 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps_start=1.0, eps_decay=.99
         if i_episode % 100 == 0:
             print(f"Episode {i_episode}/{num_episodes}, Current average step count {np.average(step_count_storage)}")
         step_count = 0
-        epsilon = 1 / i_episode
+        epsilon = 0.005
         # set the initial state
         state = env.reset()
 
@@ -33,11 +33,12 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps_start=1.0, eps_decay=.99
             next_state, reward, done, info = env.step(action)
             step_count += 1
 
-            # sarsamax uses the greedy action for the next state
+            # expected sarsa uses ...
             # update the Q table now
             current_estimate = Q[state][action]
-            greedy_estimate = Q[next_state][np.argmax(Q[next_state])] if next_state is not None else 0
-            new_estimate = current_estimate + alpha * (reward + gamma * greedy_estimate - current_estimate)
+            prop: tuple = get_probabilities(Q, next_state, epsilon)
+            expected_return = sum([prop[a] * Q[next_state][a] for a in np.arange(env.nA)])
+            new_estimate = current_estimate + alpha * (reward + gamma * expected_return - current_estimate)
             Q[state][action] = new_estimate
 
             if not done:
@@ -81,14 +82,13 @@ def get_probabilities(Q, state, epsilon) -> tuple:
 
 
 # obtain the estimated optimal policy and corresponding action-value function
-Q_sarsamax = q_learning(env, 5000, .01)
+Q_expsarsa = expected_sarsa(env, 5000, 1)
 
 # print the estimated optimal policy
-policy_sarsamax = np.array([np.argmax(Q_sarsamax[key]) if key in Q_sarsamax else -1 for key in np.arange(48)]).reshape(
-    (4, 12))
-check_test.run_check('td_control_check', policy_sarsamax)
+policy_expsarsa = np.array([np.argmax(Q_expsarsa[key]) if key in Q_expsarsa else -1 for key in np.arange(48)]).reshape(4,12)
+check_test.run_check('td_control_check', policy_expsarsa)
 print("\nEstimated Optimal Policy (UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3, N/A = -1):")
-print(policy_sarsamax)
+print(policy_expsarsa)
 
 # plot the estimated optimal state-value function
-plot_values([np.max(Q_sarsamax[key]) if key in Q_sarsamax else 0 for key in np.arange(48)])
+plot_values([np.max(Q_expsarsa[key]) if key in Q_expsarsa else 0 for key in np.arange(48)])
